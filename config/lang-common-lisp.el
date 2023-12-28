aca0e6c1b2203b6694f1f924d23d03056ebb992e
netpyoung <netpyoung@gmail.com>
2023-12-28 12:22:58 +0900

    add: custom:sly-eval-last-expression-eros

--- config/lang-common-lisp.el
+++ config/lang-common-lisp.el
@@ -7,3 +7,25 @@
   (setq inferior-lisp-program
         (string-trim
          (shell-command-to-string "which sbcl"))))
+
+(progn
+  ;; `overlay'
+  ;; ref: https://www.reddit.com/r/emacs/comments/bi4xk1/evaluation_overlays_in_slime_for_common_lisp/
+  (require 'cl-lib)
+  (require 'eros)
+  (defun custom:sly-eval-last-expression-eros ()
+    (interactive)
+    (let* ((region (sly-region-for-defun-at-point))
+           (form (apply #'buffer-substring-no-properties region))
+           (pos-end (list (- (cadr region) 1)))
+           (expr (sly-eval `(slynk:eval-and-grab-output ,form))))
+      (cl-destructuring-bind (output value) expr
+        (let ((val (concat output value)))
+          (eros--make-result-overlay value
+            :where pos-end
+            :duration eros-eval-result-duration)
+          ;; (message value)
+          (message value)))))
+  ;; (keymap-set sly-mode-map "C-M-x" 'sly-eval-defun)
+  (keymap-set sly-mode-map "C-M-x" 'custom:sly-eval-last-expression-eros))
+
